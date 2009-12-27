lambdastream/tdd_template

commit 1ee5f2d5dc9a5c4434c49d252f55200b08c53860

FUNCT fix the parser to detect @@ as a {text, "@"} terminal
This is an example of mistaken change. Tests are still failing because we
introduce an error in our "fix"

lstd_template.erl
@@ -37,6 +37,8 @@ tokens(S) ->
 parse(Tokens) ->
     parse(Tokens, text).
 
+parse([at, at| T], Terminal) ->
+    [{text, "@"} | parse(T, Terminal)];
 parse([at| T], Terminal) ->
     parse(T, switch_terminal(Terminal));
 parse([{string, S}| T], Terminal) ->

commit 2ea8a0ef7a825eb0a104317b618f71978727a2ff

BUGFIX interpret escaped ats only when parsing text
This fixed the intended mistake we introduced in

1ee5f2d5dc9a5c4434c49d252f55200b08c53860

to show how previous tests ease bug detection when adding new features

lstd_template.erl
@@ -37,7 +37,7 @@ tokens(S) ->
 parse(Tokens) ->
     parse(Tokens, text).
 
-parse([at, at| T], Terminal) ->
+parse([at, at| T], Terminal = text) ->
     [{text, "@"} | parse(T, Terminal)];
 parse([at| T], Terminal) ->
     parse(T, switch_terminal(Terminal));

lstd_template_eqc.erl
@@ -35,6 +35,14 @@ text() ->
 var() ->
     {var, valid_string(), ql_gen:string()}.
 
+%% Make it easy to generate duplicated names, but also try with wicked, weird
+%% strings
+var_name() ->
+    eqc_gen:frequency(
+      [{1, valid_string()},
+       {50, "a"},
+       {5, eqc_gen:vector(3, eqc_gen:elements(lists:seq($a, $f)))}]).
+
 %% Change sequences like [{text, "a"}, {text, "b"}] in [{text, "ab"}]
 fold_text([{text, A}, {text, B} | T]) ->
     fold_text([{text, A ++ B} | T]);